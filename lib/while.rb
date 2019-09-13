618ef3220d26cc92c9a20216ecfb3bc14e4f1617
Jalysa Reason <jalysareason@gmail.com>
2019-09-13 17:04:23 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,6 @@
 def using_while
   counter = 6
-	puts using_while ("'Wingardium Leviosa)
+	puts using_while ("'Wingardium Leviosa until the levitation force is incremented to 10")
 	counter += 1
 	end
 
